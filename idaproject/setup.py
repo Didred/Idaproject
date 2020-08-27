a3f6eac9eefeb4b35b17256d7ccf4ac1d2cac62c
Andrey_Yurevich <and9019468@yandex.ru>
2020-08-27 19:15:30 +0300

    Fixed the file setup.py

--- idaproject/setup.py
+++ idaproject/setup.py
@@ -6,7 +6,7 @@ setup(
     version='1.0',
     author="Andrey_Yurevich",
     author_email="and9019468@yandex.ru",
-    install_requires=["SQLAlchemy"],
+    install_requires=["SQLAlchemy", "django", "Pillow", "python-resize-image"],
     packages=find_packages(),
     entry_points={"console_scripts": ["idaproject = console.main:main"]},
     test_suite='library.tests'
